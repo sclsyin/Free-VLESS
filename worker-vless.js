d1c2899f2c92639e8e0d68eaae7de3f56c3c8f3c
Victor <fy123nig@gmail.com>
2024-08-03 16:56:09 +0800

    Update worker-vless.js

--- worker-vless.js
+++ worker-vless.js
@@ -4,7 +4,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
-let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
+let userID = '84a81624-b284-49d9-acb4-e4bb2f316460';
 
 let proxyIP = 'cdn.xn--b6gac.eu.org'; // workers.cloudflare.cyou bestproxy.onecf.eu.org cdn-all.xn--b6gac.eu.org cdn.xn--b6gac.eu.org
 
